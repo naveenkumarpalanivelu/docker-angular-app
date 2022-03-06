6c4ccde2f44babc9f30c10b0474d322b4bb210c5
naveenkumar.p <naveenkumar.p@servion.com>
2022-03-06 08:04:44 +0530

    revert changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-first-docker-angular-app';
+  title = 'docker-angular-app';
 }
