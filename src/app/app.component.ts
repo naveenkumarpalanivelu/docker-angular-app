98d147c7d0a9fe539c236f475c0089e66a7862f0
naveenkumar.p <naveenkumar.p@servion.com>
2022-03-06 07:40:06 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'docker-angular-app';
+  title = 'my-first-docker-angular-app';
 }
