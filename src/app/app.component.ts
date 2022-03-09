f7ffb4aa03142b42381814b57c2c2b1f167437d5
naveenkumar.p <naveenkumar.p@servion.com>
2022-03-09 09:40:19 +0530

    change app title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,7 +10,7 @@ import { Router, ActivatedRoute } from '@angular/router';
 })
 
 export class AppComponent {
-  title = 'dnc';
+  title = 'sample angular app';
 
   constructor(private router: Router,) {
   }
